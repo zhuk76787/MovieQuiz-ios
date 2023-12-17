a0da94c97255f98c5817c8b7cb345f277f2c8207
zhuk76787 <zhuk76787@gmail.com>
2023-12-17 20:48:43 +0400

    изменен вывод в консоль в файле AppDelegate

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -7,7 +7,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         return true
-        print("Hello World for branch to merge")
+        print("Hello World")
     }
 
     // MARK: UISceneSession Lifecycle
