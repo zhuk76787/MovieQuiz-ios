934248424171a49b6f38c4b5b80d120d931e1139
zhuk76787 <zhuk76787@gmail.com>
2024-02-20 14:31:43 +0400

    вынес метод convert() из MoveQuizViewController в Presenter

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -2,13 +2,14 @@ import UIKit
 
 final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, AlertPresenterDelegate {
     
-    private var currentQuestionIndex = 0
+    
     private var correctAnswers = 0
     
-    private let questionsAmount: Int = 10
+   
     private var currentQuestion: QuizQuestion?
     private var questionFactory: QuestionFactoryProtocol?
     private var alertPresenter: AlertPresenterProtocol?
+    private let presenter = MovieQuizPresenter()
     private var statisticService: StatisticServiceProtocol = StatisticServiceImplementation()
     
     
@@ -42,18 +43,11 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, 
     func didReceiveNextQuestion(question: QuizQuestion?) {
         guard let question = question else {return}
         currentQuestion = question
-        let viewModel = convert(model: question)
+        let viewModel = presenter.convert(model: question)
         DispatchQueue.main.async { [weak self] in
             self?.show(quiz: viewModel)
         }
         
-    }
-    
-    private func convert(model: QuizQuestion) -> QuizStepViewModel {
-        return QuizStepViewModel(
-            image: UIImage(data: model.image) ?? UIImage(),
-            question: model.text,
-            questionNumber: "\(currentQuestionIndex + 1)/\(questionsAmount)")
     }
     
     func show(alert: UIAlertController) {
@@ -74,7 +68,7 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, 
             buttonText: result.buttonText,
             buttonAction: {[weak self] in
                 guard let self = self else {return}
-                self.currentQuestionIndex = 0
+                self.presenter.resetQuestionIndex()
                 self.correctAnswers = 0
                 self.noButton.isEnabled = true
                 self.yesButton.isEnabled = true
@@ -99,13 +93,13 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, 
     }
     
     private func showNextQuestionOrResults() {
-        if currentQuestionIndex == questionsAmount - 1 {
-            statisticService.store(correct: correctAnswers, total: questionsAmount)
+        if presenter.isLastQuestion() {
+            statisticService.store(correct: correctAnswers, total: presenter.questionsAmount)
             let quizCount = statisticService.gamesCount
             let bestGame = statisticService.bestGame
             let formattedAccuracy = String(format: "%.0f%%", statisticService.totalAccuracy * 100)
             let text = """
-                                    Ваш результат: \(correctAnswers)/\(questionsAmount)
+                                    Ваш результат: \(correctAnswers)/\(presenter.questionsAmount)
                                     Количество сыгранных квизов: \(quizCount)
                                     Рекорд: \(bestGame.correct)/\(bestGame.total) (\(bestGame.date.dateTimeString))
                                     Средняя точность: \(formattedAccuracy)
@@ -117,7 +111,7 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, 
                 buttonText: "Сыграть еще раз")
             show(quiz: results)
         } else {
-            currentQuestionIndex += 1
+            presenter.switchToNextQuestion()
             questionFactory?.requestNextQuestion()
             noButton.isEnabled = true
             yesButton.isEnabled = true
@@ -152,12 +146,13 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, 
                                buttonText: "Попробовать еще раз") { [weak self] in
             guard let self = self else {return}
             
-            self.currentQuestionIndex = 0
+            self.presenter.resetQuestionIndex()
             self.correctAnswers = 0
             
             self.questionFactory?.requestNextQuestion()
         }
         alertPresenter?.show(alertModel: model)
+       
     }
     
     func didLoadDataFromServer() {
